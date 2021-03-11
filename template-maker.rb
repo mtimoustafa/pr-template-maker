d4db45da9597cf4ede5ebd74792e9ef1d8a9a9a2
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2021-03-11 15:59:42 -0500

    Give ERB parsing a shot

--- template-maker.rb
+++ template-maker.rb
@@ -1,6 +1,15 @@
 #!/usr/bin/ruby
 
+require 'erb'
 require 'tty-command'
 
 command = TTY::Command.new
-command.run('git pull')
+erb = ERB.new(File.read('./template.md.erb'))
+
+template_options = {
+  :name => 'Mohamed'
+}
+
+# TODO: check if hub is installed
+# command.run('hub pull-request -F')
+command.run('echo', erb.result(binding))
